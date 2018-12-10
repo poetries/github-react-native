d28c37f1a8a6441cadf14da65387c40e8a92d816
poetries <jingguanliuye@gmail.com>
2018-12-10 14:11:53 +0800

    Create index.js

---
+++ src/redux/actions/index.js
@@ -0,0 +1,6 @@
+export const MSG_SHOW = 'MSG_SHOW'
+export const MSG_INIT = 'MSG_INIT'
+export const COMMON_FETCHING = 'COMMON_FETCHING'
+export const COMMON_OVER = 'COMMON_OVER'
+
+export * from './fetchGithubList'
